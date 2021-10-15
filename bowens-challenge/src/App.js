8dda317feeb32601dcf59b193acd20f0ec0a1d5b
Minjin chou <s3641315@student.rmit.edu.au>
2021-10-16 01:12:02 +1100

    Created header with title
    -Header with similar gradient color to theme
    - Further refine formatting of movies on grid.

--- bowens-challenge/src/App.js
+++ bowens-challenge/src/App.js
@@ -1,28 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import Movie from './components/Movies';
+import Movie from "./components/Movies";
 
 // API Urls
-import MoviesApi from './config/MoviesApi';
+import MoviesApi from "./config/MoviesApi";
 
 function App() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(MoviesApi.getMoviesUrl).then((res) => res.json())
-                                 .then((data) => {
-                                   console.log(data);
-                                   setMovies(data.results);
-                                 });
-
+    fetch(MoviesApi.getMoviesUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
   }, []);
   return (
-    <div className="movie-container">
-      {movies.length > 0 && movies.map((movie) => 
-        <Movie key={movie.id} {...movie}/>
-      )}
+    <div className="website">
+      <div className="movie-image">
+        <div className="movie-title">
+          <h1>Movie</h1>
+          <br />
+        </div>
+      </div>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
     </div>
-    
   );
 }
 
